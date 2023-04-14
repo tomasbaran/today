3ad3b7404a0ffcf880c67bc55c3149c202be1258
Tomas Baran <tomas.baran@gmail.com>
2023-04-13 19:30:24 -0600

    Getting to the latest commit

--- lib/screens/tasks_screen/tasks_screen_manager.dart
+++ lib/screens/tasks_screen/tasks_screen_manager.dart
@@ -5,7 +5,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:googleapis/tasks/v1.dart';
-import 'package:today/models/dumby_data.dart';
 import 'package:today/models/my_task.dart';
 import 'package:today/models/my_list.dart';
 import 'package:today/services/calendar_service.dart';
@@ -14,17 +13,27 @@ import 'package:today/services/task_service.dart';
 class TodayScreenManager {
   final listNotifier = ValueNotifier<MyList>(MyList());
 
+  addTask({DateTime? date, String? listId}) {}
+
   getList({DateTime? date, String? listId}) {
-    log('getList for: $date');
     MyList myList = MyList();
+
+    if (date != null) {
+      log('getList for: $date');
+      myList.title = date.toString();
+    }
+
+    if (listId != null) {
+      myList.title = listId;
+    }
+
     TaskService().getList(date: date)?.onData((data) {
-      final listData = data.data();
+      final dbList = data.data();
       int i = 0;
 
-      if (listData != null && listData.isNotEmpty) {
-        myList.title = listData['title'];
-        print('listData: $listData');
-        for (var task in listData['tasks']) {
+      if (dbList != null && dbList.isNotEmpty) {
+        print('listData: $dbList');
+        for (var task in dbList['tasks']) {
           print('task: $task');
           print("id: ${task['id']}, title: ${task['title']}");
           myList.items.add(MyTask(
