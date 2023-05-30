1ba249c3ab8e4bc8986c9ec81336c80c9003856a
Tomas Baran <tomas.baran@gmail.com>
2023-05-30 15:00:33 -0600

    Implement showCupertinoModalBottomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:today/screens/tasks_screen/tasks_screen.dart';
 import 'package:today/services/auth.dart';
 import 'package:today/services/service_locator.dart';
@@ -41,7 +42,17 @@ class TodayApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Today',
-      home: Auth().uid == null ? LoginScreen() : const TasksScreen(),
+      // home: Auth().uid == null ? LoginScreen() : const TasksScreen(),
+      // onGenerateRoute (instead of home) is necessary for the showCupertinoModalBottomSheet to animate and shrink the background when adding a new task
+      onGenerateRoute: (RouteSettings settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialWithModalsPageRoute(
+              builder: (_) => Auth().uid == null ? LoginScreen() : const TasksScreen(),
+              settings: settings,
+            );
+        }
+      },
     );
   }
 }
--- lib/screens/tasks_screen/tasks_screen.dart
+++ lib/screens/tasks_screen/tasks_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:today/screens/tasks_screen/tasks_screen_manager.dart';
 import 'package:today/services/service_locator.dart';
 import 'package:today/style/style_constants.dart';
 import 'package:today/widgets/sliver_app_bar_widget.dart';
 import 'package:today/widgets/tasks_container.dart';
+import 'package:today/widgets/add_new_task_sheet.dart';
 
 class TasksScreen extends StatefulWidget {
   const TasksScreen({Key? key}) : super(key: key);
@@ -25,7 +27,15 @@ class _TasksScreenState extends State<TasksScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => widgetManager.addTaskToDateList(),
+        // onPressed: () => widgetManager.addTaskToDateList(),
+        onPressed: () {
+          showCupertinoModalBottomSheet(
+            context: context,
+            builder: (context) => Scaffold(
+              body: AddNewTaskSheet(),
+            ),
+          );
+        },
         label: const Text('+ Add task'),
       ),
       body: NestedScrollView(
