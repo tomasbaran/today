24cc41d5c14623ed8402d9dce6b0ace1191ea911
Tomas Baran <tomas.baran@gmail.com>
2023-06-08 16:30:44 -0600

    Refactor removal of NestedScroll: part 2

--- lib/screens/tasks_screen/tasks_screen.dart
+++ lib/screens/tasks_screen/tasks_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:today/screens/tasks_screen/tasks_screen_manager.dart';
 import 'package:today/services/service_locator.dart';
 import 'package:today/style/style_constants.dart';
 import 'package:today/widgets/floating_container.dart';
-import 'package:today/widgets/sliver_app_bar_widget.dart';
 import 'package:today/widgets/tasks_container.dart';
 import 'package:today/widgets/add_new_task_sheet.dart';
 
@@ -27,24 +27,29 @@ class _TasksScreenState extends State<TasksScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        // shadowColor: Colors.transparent,
+        title: ValueListenableBuilder(
+          valueListenable: widgetManager.selectedList,
+          builder: ((_, selectedList, __) => Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    selectedList.title ?? '...',
+                    style: collapsedAppBarTitleTextStyle,
+                  ),
+                  Text(
+                    selectedList.date == null ? '?' : DateFormat.MMMMd('en_US').format(selectedList.date!),
+                    style: collapsedAppBarSubtitleTextStyle,
+                  ),
+                ],
+              )),
+        ),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingContainer(),
-      body: NestedScrollView(
-        controller: parentScrollController,
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverOverlapAbsorber(
-              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-              sliver: const SliverAppBar(
-                pinned: true,
-                expandedHeight: expandedAppBarHeight,
-                flexibleSpace: SliverAppBarWidget(),
-              ),
-            ),
-          ];
-        },
-        body: TasksContainer(),
-      ),
+      body: TasksContainer(),
     );
   }
 }
